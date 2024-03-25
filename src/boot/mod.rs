93b2ec8d0173af2133049340a3c77e6b6ed3ee34
Alexander Ulmer <alexander.ulmer@gurdinet.at>
2024-03-25 13:58:11 +0100

    Multiboot: Cleanup

--- src/boot/mod.rs
+++ src/boot/mod.rs
@@ -1,6 +1,6 @@
-//! Boot protocol implementation(s). Usually, the bootloader (e.g. GRUB) loads the kernel into
-//! memory and jumps to it. A boot protocol (e.g. multiboot) is used to define the machine state at
-//! the point where the kernel is entered.
+//! Kernel entry point and boot protocol implementation(s). Usually, the bootloader (e.g. GRUB)
+//! loads the kernel into memory and jumps to it. The boot protocol (e.g. multiboot) defines the
+//! machine state at that point and any other information that the bootloader passes to the kernel.
 
 mod multiboot;
 
@@ -17,47 +17,30 @@ static MULTIBOOT_HEADER: multiboot::Header = multiboot::Header::new()
 /// The base address of the boot stack. The stack grows downwards from this address.
 const BOOT_STACK_BASE: usize = 0x8_0000;
 
-/// Entry point into the kernel in case the `multiboot` boot protocol is being used. The multiboot
-/// specification defines the machine state at this point:
-///
-/// * `eax`: Must contain `0x2BADB002`. This value indicates to the kernel that it was loaded by a
-/// multiboot-compliant bootloader.
-/// * `ebx`: Contains the 32-bit physical address of the multiboot information structure provided
-/// by the boot loader.
-/// * `cs`: Contains valid read/exec code segment selector with an offset of `0` and a limit of
-/// `0xffff_ffff`.
-/// * `ds`, `es`, `fs`, `gs`: Contain valid read/write data segment selectors with an offset of `0`
-/// and a limit of `0xffff_ffff`.
-/// * `cr0`: PG (paging) disabled, PE (protected mode) enabled
-/// * `eflags`: VM clear, IF clear (interrupts disabled)
-/// * `esp`: Undefined. We need to load our own stack.
-/// * `gdtr`: Undefined. We can't load segment registers until we install our own global descriptor
-/// table.
-/// * `idtr`:   Undefined. We must install our own interrupt descriptor table.
+/// Entry point into the kernel in case the `multiboot` boot protocol is being used. This will
+/// prepare the processor to run Rust code before it jumps to the [`multiboot_start()`] function.
 #[naked]
 #[no_mangle]
 unsafe extern "C" fn _multiboot_entry() {
+    // Exact machine state at this point is defined by the multiboot specification.
+    // * `eax`: Must contain magic value `0x2BADB002`.
+    // * `ebx`: Contains the physical address of the multiboot information structure.
+    // * `esp`: Stack pointer is in an undefined state. We must load our own.
     core::arch::asm!(
-        // Bootloader leaves `%esp` undefined. We must setup our own stack.
         "mov ${stack_ptr}, %esp",
-        // Call `multiboot_start()` function and pass on the magic value and multiboot struct
-        // pointer left behind by the bootloader.
-        "push %ebx", // 2nd argument
-        "push %eax", // 1st argument
+        "push %ebx", // 2nd argument to `multiboot_start()`
+        "push %eax", // 1st argument to `multiboot_start()`
+        "call clear_bss",
         "call multiboot_start",
         stack_ptr = const { BOOT_STACK_BASE },
         options(att_syntax, noreturn)
     );
 }
 
+/// Rust entry point into the kernel after a stack has been setup.
 #[no_mangle]
 extern "C" fn multiboot_start(magic: u32, mb_ptr: *const multiboot::BootInfo) -> ! {
     use log::{debug, info};
-
-    // Safety: This must be done before any relevant statics are accessed.
-    unsafe {
-        clear_bss();
-    }
 
     crate::logging::initialize_kernel_log();
     info!("Kernel by Alexander Ulmer v{}", env!("CARGO_PKG_VERSION"));
@@ -95,7 +78,8 @@ extern "C" fn multiboot_start(magic: u32, mb_ptr: *const multiboot::BootInfo) ->
 /// function is executed after data in the BSS segment has been mutated. Furthermore, this function
 /// assumes that the symbols `__bss_start` and `_bss_end` defined in the linker script are valid and
 /// well-aligned addresses.
-pub unsafe fn clear_bss() {
+#[no_mangle]
+unsafe extern "C" fn clear_bss() {
     // Symbols defined by linker script:
     extern "C" {
         /// Start address of the BSS segment.
@@ -105,9 +89,11 @@ pub unsafe fn clear_bss() {
         static __bss_end: u8;
     }
 
-    core::slice::from_mut_ptr_range(core::ops::Range {
-        start: (&__bss_start as *const u8).cast_mut(),
-        end: (&__bss_end as *const u8).cast_mut(),
-    })
-    .fill(0);
+    unsafe {
+        core::slice::from_mut_ptr_range(core::ops::Range {
+            start: (&__bss_start as *const u8).cast_mut(),
+            end: (&__bss_end as *const u8).cast_mut(),
+        })
+        .fill(0);
+    }
 }
--- src/boot/multiboot.rs
+++ src/boot/multiboot.rs
@@ -1,17 +1,9 @@
-//! This module contains the structures used to implement the multiboot boot protocol. It provides
-//! the kernel with parameters and information required to boot the system (e.g. system memory map,
-//! the command line and pointers to loaded modules. Note that multiboot can only load 32-bit
-//! kernels.
+//! This module contains the structures used to implement the multiboot boot protocol as defined in
+//! the corresponding specification:
 //!
-//! This source file contains verbatim copies of the multiboot specification in version 0.6.96 for
-//! which permission is granted to make and distribute verbatim copies provided the copyright notice
-//! and this permission notice are preserved on all copies:
-//!
-//! * Copyright © 1995,96 Bryan Ford <baford@cs.utah.edu>
-//! * Copyright © 1995,96 Erich Stefan Boleyn <erich@uruk.org>
-//! * Copyright © 1999,2000,2001,2002,2005,2006,2009,2010 Free Software Foundation, Inc.
+//! https://www.gnu.org/software/grub/manual/multiboot/multiboot.html (version 0.6.96)
 
-// Only compile this module on `x86` (IA-32) architecture
+// Multiboot is only specified for `x86` (IA-32) architecture
 #![cfg(target_arch = "x86")]
 
 use crate::mem::{MemoryChunk, MemoryChunkClass};
@@ -67,15 +59,11 @@ impl Header {
 }
 
 /// When the bootloader (e.g. GRUB) transfers control to the kernel, an instance of this struct is
-/// passed along to the kernel. It contains essential information with respect to the boot process
-/// (memory map, command line).
+/// passed along to the kernel. It contains information vital to the kernel startup procedure.
 #[repr(C, packed)]
 pub struct BootInfo {
     /// Indicates the presence and validity of other fields in the Multiboot information structure.
-    /// All as-yet-undefined bits must be set to zero by the boot loader. Any set bits that the
-    /// operating system does not understand should be ignored. Thus, the `flags` field also
-    /// functions as a version indicator, allowing the Multiboot information structure to be
-    /// expanded in the future without breaking anything.
+    /// Any set bits that the operating system does not understand should be ignored.
     flags: u32,
 
     /// If bit 0 in the `flags` word is set, then the `mem_lower` field is valid. `mem_lower`
@@ -90,43 +78,13 @@ pub struct BootInfo {
     mem_upper: u32,
 
     /// If bit 1 in the `flags` word is set, then the `boot_device` field is valid, and indicates
-    /// which BIOS disk device the boot loader loaded the OS image from. If the OS image was not
-    /// loaded from a BIOS disk, then this field must not be present (bit 3 must be clear). The
-    /// operating system may use this field as a hint for determining its own root device, but is
-    /// not required to. The `boot_device` field is laid out in four one-byte subfields as follows:
-    ///
-    /// ```text
-    /// +-------+-------+-------+-------+
-    /// | part3 | part2 | part1 | drive |
-    /// +-------+-------+-------+-------+
-    /// Least significant             Most significant
-    /// ```
-    ///
-    /// The most significant byte contains the BIOS drive number as understood by the BIOS INT 0x13
-    /// low-level disk interface: e.g. 0x00 for the first floppy disk or 0x80 for the first hard
-    /// disk.
-    //
-    /// The three remaining bytes specify the boot partition. `part1` specifies the top-level
-    /// partition number, `part2` specifies a sub-partition in the top-level partition, etc.
-    /// Partition numbers always start from zero. Unused partition bytes must be set to 0xFF. For
-    /// example, if the disk is partitioned using a simple one-level DOS partitioning scheme, then
-    /// `part1` contains the DOS partition number, and `part2` and `part3` are both 0xFF. As
-    /// another example, if a disk is partitioned first into DOS partitions, and then one of
-    /// those DOS partitions is subdivided into several BSD partitions using BSD's "disk label"
-    /// strategy, then `part1` contains the DOS partition number, `part2` contains the BSD
-    /// sub-partition within that DOS partition, and `part3` is 0xFF.
-    ///
-    /// DOS extended partitions are indicated as partition numbers starting from 4 and increasing,
-    /// rather than as nested sub-partitions, even though the underlying disk layout of extended
-    /// partitions is hierarchical in nature. For example, if the boot loader boots from the second
-    /// extended partition on a disk partitioned in conventional DOS style, then `part1` will be 5,
-    /// and `part2` and `part3` will both be 0xFF.
+    /// which BIOS disk device the boot loader loaded the OS image from. The operating system may
+    /// use this field as a hint for determining its own root device, but is not required to.
     _boot_device: u32,
 
     /// If bit 2 of the `flags` word is set, the `cmdline` field is valid, and contains the
     /// physical address of the command line to be passed to the kernel. The command line is a
-    /// normal C-style zero-terminated string. The exact format of command line is left to OS
-    /// developers.
+    /// normal C-style zero-terminated string.
     cmdline: *const core::ffi::c_char,
 
     /// If bit 3 of `flags` is set, then the `mods` fields indicate to the kernel what boot
@@ -141,7 +99,7 @@ pub struct BootInfo {
     /// the [Module] structure.
     mods_addr: *const core::ffi::c_void,
 
-    _unused2: [u32; 4],
+    _unused: [u32; 4],
 
     /// If bit 6 in the `flags` word is set, then the `mmap_length` field is valid and indicates
     /// the address and length of a buffer containing a memory map of the machine provided by the
