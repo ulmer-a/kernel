6c3540fe88328ddf5039221d74ef9baba73d6f18
Alexander Ulmer <alexander.ulmer@gurdinet.at>
2024-10-27 13:34:25 +0100

    start implement paging x86

--- kernel/src/mem/paging/mod.rs
+++ kernel/src/mem/paging/mod.rs
@@ -1,14 +1,66 @@
 //! Virtual Memory and Paging
-//!
-//! We need following features
-//! * Identity map areas of physical memory to virtual memory (kernel addr space)
-//! * Map single page for user requests
-//! * ...
 
 #[cfg(target_arch = "x86")]
 mod x86;
 
-// trait PageFrameAllocator
-// struct VirtualPage
-// struct Physical Page
-// MapOptions
+pub trait PageFrameAlloc {
+    fn alloc_page(&mut self) -> PhysicalPageNumber;
+}
+
+// pub trait PagingMode {
+//     type TopLevelTable: PageTable;
+// }
+
+/// A virtual memory address space.
+pub trait AddressSpace {
+    // ...
+
+    /// Switch to this address space on the current CPU.
+    fn load();
+
+    // /// Call this as soon as the address space has been unloaded to release locks.
+    // fn unloaded();
+}
+
+pub struct UserAddressSpace {
+    // ...
+}
+
+impl AddressSpace for UserAddressSpace {
+    fn load() {
+        todo!()
+    }
+}
+
+// pub trait PageTable: core::ops::Index<VirtualPageNumber, Output = dyn PageTableEntry> {
+//     type Entry: PageTableEntry;
+
+//     fn granularity() -> usize;
+
+//     fn iter_mut(&mut self) -> impl Iterator<Item = &mut dyn PageTableEntry>;
+// }
+
+// pub trait PageTableEntry {
+//     fn map_page(&mut self, ppn: ()) -> Result<(), ()>;
+
+//     fn unmap(&mut self) -> Result<PhysicalPageMut, ()> {
+//         Err(())
+//     }
+// }
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct PhysicalPageNumber {
+    ppn: usize,
+}
+
+// impl PhysicalPageMut {
+//     pub unsafe fn get_physical_ptr<T>(&self) -> *mut T {
+//         // (ppn * PAGE_SIZE) as *mut T
+//         todo!()
+//     }
+// }
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct VirtualPageNumber {
+    vpn: usize,
+}
--- kernel/src/mem/paging/x86.rs
+++ kernel/src/mem/paging/x86.rs
@@ -1,1 +1,107 @@
+//! Paging/virtual memory implementation for IA-32 architecture (x86).
 
+use super::PageFrameAlloc;
+
+pub struct BootIdentMapping {}
+
+impl BootIdentMapping {
+    pub fn new(
+        allocator: &mut dyn PageFrameAlloc,
+        mappings: impl Iterator<Item = types::mem::MemoryChunk>,
+    ) -> Self {
+        let mut builder = BootIdentMappingBuilder::new(allocator);
+        for mapping in mappings {
+            builder.add_mapping(mapping);
+        }
+        Self {}
+    }
+}
+
+pub struct BootIdentMappingBuilder<'alloc> {
+    allocator: &'alloc mut dyn PageFrameAlloc,
+    page_directory: *mut (),
+}
+
+impl<'alloc> BootIdentMappingBuilder<'alloc> {
+    fn new(allocator: &'alloc mut dyn PageFrameAlloc) -> Self {
+        let page_directory = allocator.alloc_page();
+        Self {
+            allocator,
+            page_directory: unsafe { todo!() },
+        }
+    }
+
+    fn add_mapping(&mut self, region: types::mem::MemoryChunk) {
+        let page_dir = unsafe { self.page_directory.as_mut().unwrap() };
+        todo!()
+    }
+}
+
+// type PageDirectory = PageTable32<2>;
+
+// type PageTable = PageTable32<1>;
+
+// #[repr(C, align(4096))]
+// struct PageTable32<const LEVEL: usize> {
+//     tables: [u32; 1024],
+// }
+
+// struct PageTableIterMut<'table, T> {
+//     entries: &'table mut [u32],
+// }
+
+// impl<'table, T: super::PageTable> Iterator for PageTableIterMut<'table, T> {
+//     type Item = &'table mut dyn super::PageTableEntry;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let entry = &mut self.entries.get(0)?;
+//         self.entries = &mut self.entries[1..];
+//         Some(unsafe { core::mem::transmute(entry) })
+//     }
+// }
+
+// impl super::PageTable for PageDirectory {
+//     fn granularity() -> usize {
+//         4096 * 1024
+//     }
+
+//     fn iter_mut(&mut self) -> impl Iterator<Item = &mut dyn super::PageTableEntry> {
+//         todo!()
+//     }
+// }
+
+// impl super::PageTable for PageTable {
+//     fn granularity() -> usize {
+//         4096
+//     }
+
+//     fn iter_mut(&mut self) -> impl Iterator<Item = &mut dyn super::PageTableEntry> {
+//         todo!()
+//     }
+// }
+
+// struct PageTableEntry32<T> {
+//     inner: u32,
+//     _phantom: core::marker::PhantomData<T>,
+// }
+
+// impl<T> From<u32> for PageTableEntry32<T> {
+//     fn from(value: u32) -> Self {
+//         Self {
+//             inner: value,
+//             _phantom: core::marker::PhantomData,
+//         }
+//     }
+// }
+
+// impl super::PageTableEntry for PageTableEntry32<PageDirectory> {
+//     fn map_page(&mut self, _ppn: ()) -> Result<(), ()> {
+//         todo!()
+//     }
+// }
+
+// impl super::PageTableEntry for PageTableEntry32<PageTable> {
+//     fn map_page(&mut self, _ppn: ()) -> Result<(), ()> {
+//         todo!()
+//     }
+// }
